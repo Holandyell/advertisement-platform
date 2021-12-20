598340d3371ec0feb972de224a86bc5b808c5cdd
vazha <vazha@holandyell.home>
2021-12-20 07:53:40 -0500

    Fixed templates html structure

--- add.php
+++ add.php
@@ -7,41 +7,44 @@ $locations = (new LocationRepository())->getLocationList();
 include 'Template/html_start.php';
 ?>
 
-<form class="mt-4" method="POST" action="App/create_ad_submit.php">
-    <div class="form-group">
-        <label for="title">Title</label>
-        <input type="text" class="form-control" id="title" name="title" autocomplete="off" placeholder="Enter the title">
-    </div>
-    <div class="form-group">
-        <label for="category">Category</label>
-        <select class="form-control" id="category" name="category">
-            <?php foreach ($categories as $row) { ?>
-                <option value="<?=$row['id']?>"><?=$row['title']?></option>
-            <?php } ?>
-        </select>
-    </div>
-    <div class="form-group">
-        <label for="location">Location</label>
-        <select class="form-control" id="location" name="location">
-            <?php foreach ($locations as $row) { ?>
-                <option value="<?=$row['id']?>"><?=$row['title']?></option>
-            <?php } ?>
-        </select>
-    </div>
-    <div class="form-group">
-        <label for="price">Price</label>
-        <input type="text" class="form-control" id="price" name="price" autocomplete="off" placeholder="Enter the price">
-    </div>
-    <div class="form-group">
-        <label for="description">Description</label>
-        <textarea id="description" name="description" class="form-control"></textarea>
-    </div>
-    <div class="form-group">
-        <label for="thumbnail">Thumbnail</label>
-        <input type="text" class="form-control" id="thumbnail" name="thumbnail" autocomplete="off" placeholder="Enter the thumbnail url">
-    </div>
-    <a type="submit" href="/example/submit.php" class="btn btn-primary mt-2">Create</a>
-    <a href="/" class="btn btn-secondary mt-2">Cancel</a>
-</form>
+<div class="container">
+    <form class="mt-4" method="POST" action="App/create_ad_submit.php">
+        <div class="form-group">
+            <label for="title">Title</label>
+            <input type="text" class="form-control" id="title" name="title" autocomplete="off" placeholder="Enter the title">
+        </div>
+        <div class="form-group">
+            <label for="category">Category</label>
+            <select class="form-control" id="category" name="category">
+                <?php foreach ($categories as $row) { ?>
+                    <option value="<?=$row['id']?>"><?=$row['title']?></option>
+                <?php } ?>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="location">Location</label>
+            <select class="form-control" id="location" name="location">
+                <?php foreach ($locations as $row) { ?>
+                    <option value="<?=$row['id']?>"><?=$row['title']?></option>
+                <?php } ?>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="price">Price</label>
+            <input type="text" class="form-control" id="price" name="price" autocomplete="off" placeholder="Enter the price">
+        </div>
+        <div class="form-group">
+            <label for="description">Description</label>
+            <textarea id="description" name="description" class="form-control"></textarea>
+        </div>
+        <div class="form-group">
+            <label for="thumbnail">Thumbnail</label>
+            <input type="text" class="form-control" id="thumbnail" name="thumbnail" autocomplete="off" placeholder="Enter the thumbnail url">
+        </div>
+        <a type="submit" href="/example/submit.php" class="btn btn-primary mt-2">Create</a>
+        <a href="/example/" class="btn btn-secondary mt-2">Cancel</a>
+    </form>
+</div><!-- /container -->
 
-<?php include 'Template/html_end.php'; ?>
+<?php
+include 'Template/html_end.php';
--- index.php
+++ index.php
@@ -70,93 +70,95 @@ $mark = (new MarkRepository())->getMarkList($markId);
 include 'Template/html_start.php';
 
 ?>
-<div class="mt-4">
-    <a class="btn btn-primary btn-sm" href="/example/add.php">Create</a>
-</div>
 
-<div class="row">
-    <div class="col-9">
-        <?php
-        foreach ($ads as $row) {
-            if ($row['thumbnail']) {
-                $thumbnail = $row['thumbnail'];
-            } else {
-                $thumbnail = 'https://st4.depositphotos.com/14953852/24787/v/600/depositphotos_247872612-stock-illustration-no-image-available-icon-vector.jpg';
+<div class="container">
+    <div class="mt-4">
+        <a class="btn btn-primary btn-sm" href="/example/add.php">Create</a>
+    </div>
+
+    <div class="row">
+        <div class="col-8">
+            <?php
+            foreach ($ads as $row) {
+                if ($row['thumbnail']) {
+                    $thumbnail = $row['thumbnail'];
+                } else {
+                    $thumbnail = 'https://st4.depositphotos.com/14953852/24787/v/600/depositphotos_247872612-stock-illustration-no-image-available-icon-vector.jpg';
+                }
+                ?>
+                <div class="row mt-4">
+                    <div class="col-3">
+                        <img style="max-width: 180px;"
+                            src="<?php echo $thumbnail; ?>">
+                    </div>
+                    <div class="col-9">
+                        <h3><?=$row['title']?></h3>
+                        <div>Location: <?=$row['location_title']?></div>
+                        <div>Category: <?=$row['category_title']?></div>
+                        <div>Price: <?=$row['price']?></div>
+                        <p><?=$row['description']?></p>
+                    </div>
+                </div>
+                <?php
             }
             ?>
-            <div class="row mt-4">
-                <div class="col-3">
-                    <img style="max-width: 180px;"
-                        src="<?php echo $thumbnail; ?>">
-                </div>
-                <div class="col-9">
-                    <h3><?=$row['title']?></h3>
-                    <div>Location: <?=$row['location_title']?></div>
-                    <div>Category: <?=$row['category_title']?></div>
-                    <div>Price: <?=$row['price']?></div>
-                    <p><?=$row['description']?></p>
-                </div>
-            </div>
-            <?php
-        }
-        ?>
-    </div>
-    <div class="col-3">
-        <h3>Filters</h3>
+        </div>
+        <div class="col-4">
+            <h3>Filters</h3>
 
-        <form class="mt-4" method="GET" action="">
-            <div class="form-group">
-                <label  for="search">Search</label>
-                <input type="text" style="margin-left: -100px;" class="form-control" id="search" name="search" autocomplete="off" placeholder="Search" value="<?=$search?>">
-            </div>
-            <div class="form-group">
-                <label for="category">Category</label>
-                <select style="margin-left: -100px;" class="form-control" id="category" name="category">
-                    <option value=""></option>
-                    <?php foreach ($categories as $row) { ?>
-                        <option value="<?=$row['id']?>" <?php if ($categoryId == $row['id']) { echo 'selected'; } ?>><?=$row['title']?></option>
-                    <?php } ?>
-                </select>
-            </div>
-            <div class="form-group">
-                <label for="location">Location</label>
-                <select style="margin-left: -100px;" class="form-control" id="location" name="location">
-                    <option value=""></option>
-                    <?php foreach ($locations as $row) { ?>
-                        <option value="<?=$row['id']?>" <?php if ($locationId == $row['id']) { echo 'selected'; } ?>><?=$row['title']?></option>
-                    <?php } ?>
-                </select>
-            </div>
-            <div class="form-group">
-                <div>
-                    <label>Price</label>
+            <form class="mt-4" method="GET" action="">
+                <div class="form-group">
+                    <label  for="search">Search</label>
+                    <input type="text" class="form-control" id="search" name="search" autocomplete="off" placeholder="Search" value="<?=$search?>">
                 </div>
-                <div class="row" style="padding: 0 15px;">
-                    <input class="form-control col-6" style="margin-left: -70px;" id="min_price" name="min_price" placeholder="to" autocomplete="off" value="<?=$from?>" >
-                    <input class="form-control col-6"  style="margin-left: -50px;"id="max_price" name="max_price" placeholder="From"  autocomplete="off" value="<?=$to?>">
+                <div class="form-group">
+                    <label for="category">Category</label>
+                    <select class="form-control" id="category" name="category">
+                        <option value=""></option>
+                        <?php foreach ($categories as $row) { ?>
+                            <option value="<?=$row['id']?>" <?php if ($categoryId == $row['id']) { echo 'selected'; } ?>><?=$row['title']?></option>
+                        <?php } ?>
+                    </select>
                 </div>
-            </div>
-            <div class="form-group">
-                <div>
-                    <label>Mark</label>
+                <div class="form-group">
+                    <label for="location">Location</label>
+                    <select class="form-control" id="location" name="location">
+                        <option value=""></option>
+                        <?php foreach ($locations as $row) { ?>
+                            <option value="<?=$row['id']?>" <?php if ($locationId == $row['id']) { echo 'selected'; } ?>><?=$row['title']?></option>
+                        <?php } ?>
+                    </select>
                 </div>
-                    <select style="margin-left: -100px;" class="form-control" id="mark" name="mark" >
+                <div class="form-group">
+                    <div>
+                        <label>Price</label>
+                    </div>
+                    <div class="row" style="padding: 0 15px;">
+                        <input class="form-control col-6" id="min_price" name="min_price" placeholder="to" autocomplete="off" value="<?=$from?>" >
+                        <input class="form-control col-6" id="max_price" name="max_price" placeholder="From"  autocomplete="off" value="<?=$to?>">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div>
+                        <label>Mark</label>
+                    </div>
+                    <select class="form-control" id="mark" name="mark" >
                         <option value=""></option>
                         <?php foreach ($mark as $row) { ?>
                             <option value="<?=$row['id']?>" <?php if ($markId == $row['id']) { echo 'selected'; } ?>><?=$row['title']?></option>
-                    <?php } ?>
-                <div>
+                        <?php } ?>
                     <select> 
                 </div>
-            </div>
-            <div class="form-group">
-            <button type="submit" class="btn btn-success mt-2">Apply</button>
-            <a href="/example" class="btn btn-secondary mt-2">Clear</a>
-            <a href="/example/admin.php" class="btn btn-secondary mt-2">Admin</a>
-        </form>
+                <div class="form-group">
+                    <button type="submit" class="btn btn-success mt-2">Apply</button>
+                    <a href="/example" class="btn btn-secondary mt-2">Clear</a>
+                    <a href="/example/admin.php" class="btn btn-secondary mt-2">Admin</a>
+                </div>
+            </form>
 
-    </div>
-</div>
+        </div>
+    </div><!-- /row -->
+</div><!-- /container -->
 
 <?php
 include 'Template/html_end.php';
